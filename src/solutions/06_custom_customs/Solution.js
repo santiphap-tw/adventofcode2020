a4855cece1ad7dfc83e08de44c544687a24f3668
IQ <santiphap.wat@gmail.com>
2020-12-07 13:17:36 +0700

    Change import order in template

--- src/solutions/06_custom_customs/Solution.js
+++ src/solutions/06_custom_customs/Solution.js
@@ -1,8 +1,8 @@
 import input01 from "./case01/input.txt";
-import input02 from "./case02/input.txt";
 import output01Part1 from "./case01/output.part1.txt";
-import output02Part1 from "./case02/output.part1.txt";
 import output01Part2 from "./case01/output.part2.txt";
+import input02 from "./case02/input.txt";
+import output02Part1 from "./case02/output.part1.txt";
 import output02Part2 from "./case02/output.part2.txt";
 
 const part1 = (input) => {
