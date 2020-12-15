b25537e65b9d1ef0a0429ed715400e8d2d697460
IQ <santiphap.wat@gmail.com>
2020-12-15 17:28:46 +0700

    Refactor some code

--- src/solutions/15_rambunctious_recitation/Solution.js
+++ src/solutions/15_rambunctious_recitation/Solution.js
@@ -5,60 +5,31 @@ import input02 from "./case02/input.txt";
 import output02Part1 from "./case02/output.part1.txt";
 import output02Part2 from "./case02/output.part2.txt";
 
-const bruteForce = (input, target) => {
-  let seq = input[0].split(",").map((i) => parseInt(i.trim()));
-  let counter = {};
-  for (let i = 0; i < target; i++) {
-    if (seq[i] !== undefined) {
-      counter[seq[i]] = [i];
-      continue;
-    }
-    const previous = seq[i - 1];
-    if (counter[previous].length === 1) {
-      seq.push(0);
-      if (!counter[0]) counter[0] = [i];
-      else counter[0] = [i, ...counter[0]];
-    } else {
-      const spoken = counter[previous][0] - counter[previous][1];
-      seq.push(spoken);
-      if (!counter[spoken]) counter[spoken] = [i];
-      else counter[spoken] = [i, ...counter[spoken]];
-    }
-  }
-  return seq[target - 1];
-};
-
-const lastPosition = (input, target) => {
+const spokenValue = (input, target) => {
   input = input[0].split(",").map((i) => parseInt(i.trim()));
   let counter = {};
   let pos = 0;
   let previous = 0;
   for (; pos < input.length; pos++) {
-    counter[input[pos]] = [pos];
+    counter[input[pos]] = [pos, pos];
     previous = input[pos];
   }
   for (; pos < target; pos++) {
-    if (counter[previous].length === 1) {
-      previous = 0;
-      if (!counter[0]) counter[0] = [pos];
-      else counter[0] = [pos, counter[0][0]];
-    } else {
-      const spoken = counter[previous][0] - counter[previous][1];
-      previous = spoken;
-      if (!counter[spoken]) counter[spoken] = [pos];
-      else counter[spoken] = [pos, counter[spoken][0]];
-    }
+    const spoken = counter[previous][0] - counter[previous][1];
+    previous = spoken;
+    if (counter[spoken] !== undefined)
+      counter[spoken] = [pos, counter[spoken][0]];
+    else counter[spoken] = [pos, pos];
   }
   return previous;
 };
 
 const part1 = (input) => {
-  return lastPosition(input, 2020);
+  return spokenValue(input, 2020);
 };
 
 const part2 = (input) => {
-  //return lastPosition(input,30000000);
-  return 0;
+  return spokenValue(input, 30000000);
 };
 
 const Solution = {
