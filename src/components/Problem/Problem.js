e093c02a3ee3f5acbc810387f73430c9d0339618
IQ <santiphap.wat@gmail.com>
2020-12-15 17:00:36 +0700

    Add custom run button instead of autorun

--- src/components/Problem/Problem.js
+++ src/components/Problem/Problem.js
@@ -9,8 +9,8 @@ import {
 const Problem = (props) => {
   const name = props.name;
   const solution = props.solution;
-  const [result1, setResult1] = useState([]);
-  const [result2, setResult2] = useState([]);
+  const [result1, setResult1] = useState(null);
+  const [result2, setResult2] = useState(null);
   const [popup, setPopup] = useState(<div />);
 
   const showCustomRun = async () => {
@@ -30,60 +30,64 @@ const Problem = (props) => {
     );
   };
 
-  useEffect(() => {
-    async function run() {
-      setResult1(null);
-      setResult2(null);
-      const execute = async (prob) => {
-        const input = await ReadFilesToArray(prob.input);
-        const output = await ReadFilesToText(prob.output);
-        let result = [];
-        const fn = prob.solution;
-        for (const index in output) {
-          if (output[index].trim() === "") {
-            result.push(
-              <Empty
-                key={prob.output + index}
-                message={"result: " + fn(input[index]).toString()}
-              />
-            );
-          } else if (fn(input[index]).toString() === output[index]) {
-            result.push(
-              <Pass
-                key={prob.output + index}
-                message={"result: " + output[index]}
-              />
-            );
-          } else
-            result.push(
-              <Fail
-                key={prob.output + index}
-                message={
-                  "expected: " +
-                  output[index] +
-                  " | got: " +
-                  fn(input[index]).toString()
-                }
-              />
-            );
-        }
-        return result;
-      };
-      const result1 = await execute(solution.part1);
-      const result2 = await execute(solution.part2);
-      setResult1(result1);
-      setResult2(result2);
-    }
-    run();
-  }, [solution]);
+  const run = async () => {
+    setResult1(null);
+    setResult2(null);
+    const execute = async (prob) => {
+      const input = await ReadFilesToArray(prob.input);
+      const output = await ReadFilesToText(prob.output);
+      let result = [];
+      const fn = prob.solution;
+      for (const index in output) {
+        if (output[index].trim() === "") {
+          result.push(
+            <Empty
+              key={prob.output + index}
+              message={"result: " + fn(input[index]).toString()}
+            />
+          );
+        } else if (fn(input[index]).toString() === output[index]) {
+          result.push(
+            <Pass
+              key={prob.output + index}
+              message={"result: " + output[index]}
+            />
+          );
+        } else
+          result.push(
+            <Fail
+              key={prob.output + index}
+              message={
+                "expected: " +
+                output[index] +
+                " | got: " +
+                fn(input[index]).toString()
+              }
+            />
+          );
+      }
+      return result;
+    };
+    const result1 = await execute(solution.part1);
+    const result2 = await execute(solution.part2);
+    setResult1(result1);
+    setResult2(result2);
+  };
 
   return (
     <tr className="table-bordered border-left-0 border-right-0">
       <td className="align-middle">{name}</td>
       <td className="align-middle">
-        {result1}/{result2}
+        {result1}
+        {result1 !== null && "/"}
+        {result2}
       </td>
       <td className="align-middle">
+        <button className="btn btn-outline-success border-0 m-1" onClick={run}>
+          <i className="material-icons-round align-middle mx-auto">
+            playlist_play
+          </i>
+        </button>
         <button
           className="btn btn-outline-success border-0 m-1"
           onClick={showCustomRun}
