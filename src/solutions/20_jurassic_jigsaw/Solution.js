54388c5ad5f1fa72e95cc6bb83c76e8da1ae2950
IQ <santiphap.wat@gmail.com>
2020-12-22 15:49:53 +0700

    Add problem 20 solution 1

--- src/solutions/20_jurassic_jigsaw/Solution.js
+++ src/solutions/20_jurassic_jigsaw/Solution.js
@@ -5,9 +5,74 @@ import input02 from "./case02/input.txt";
 import output02Part1 from "./case02/output.part1.txt";
 import output02Part2 from "./case02/output.part2.txt";
 
+const checkBorder = (border, tiles) => {
+  for (let tilesKey = 0; tilesKey < tiles.length; tilesKey++) {
+    const tile = tiles[tilesKey];
+    if (
+      tile.border.up === border ||
+      tile.border.up === border.split("").reverse().join("")
+    )
+      return true;
+    if (
+      tile.border.down === border ||
+      tile.border.down === border.split("").reverse().join("")
+    )
+      return true;
+    if (
+      tile.border.left === border ||
+      tile.border.left === border.split("").reverse().join("")
+    )
+      return true;
+    if (
+      tile.border.right === border ||
+      tile.border.right === border.split("").reverse().join("")
+    )
+      return true;
+  }
+  return false;
+};
+
 const part1 = (input) => {
-  // Your part 1 solution
-  return 0;
+  input = input.join("\n").split(/\n\s*\n/);
+  let tiles = [];
+  input.forEach((tile) => {
+    const tileId = parseInt(tile.split("\n")[0].split(":")[0].split(" ")[1]);
+    const tileInfo = tile
+      .split("\n")
+      .slice(1)
+      .map((row) => row.split("").filter((t) => t.trim().length > 0));
+    let border = {
+      up: "",
+      down: "",
+      left: "",
+      right: "",
+    };
+    for (let i = 0; i < 10; i++) {
+      border.up += tileInfo[0][i];
+      border.down += tileInfo[9][i];
+      border.left += tileInfo[i][0];
+      border.right += tileInfo[i][9];
+    }
+    tiles.push({
+      id: tileId,
+      border: border,
+    });
+  });
+  let result = 1;
+  tiles.forEach((tile) => {
+    let countNeighbours = 0;
+    Object.entries(tile.border).forEach(([direction, border]) => {
+      if (
+        checkBorder(
+          border,
+          tiles.filter((t) => t.id !== tile.id)
+        )
+      )
+        countNeighbours++;
+    });
+    if (countNeighbours === 2) result *= tile.id;
+  });
+  return result;
 };
 
 const part2 = (input) => {
